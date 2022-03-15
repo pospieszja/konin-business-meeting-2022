d1bce7ab7d06fc9833f6e840d520394e113463be
pospieszja <j.pospieszynski@outlook.com>
2022-03-15 10:58:24 +0100

    Add brand colors palette for TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#009FE3',
+          '50': '#9CE1FF',
+          '100': '#87DBFF',
+          '200': '#5ECFFF',
+          '300': '#36C3FF',
+          '400': '#0DB6FF',
+          '500': '#009FE3',
+          '600': '#0078AB',
+          '700': '#005073',
+          '800': '#00293B',
+          '900': '#000203'},
+      }
+    },
   },
   plugins: [],
 }
