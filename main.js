6436f9258126271a6a68e271219cce20af6e7850
pospieszja <j.pospieszynski@outlook.com>
2026-03-26 16:31:17 +0100

    Always highlighted 'ticket' item in menu

--- main.js
+++ main.js
@@ -30,3 +30,20 @@ for (var i = 0; i < navLink.length; i++) {
     button.classList.toggle('hidden');    
   });
 }
+
+const nav = document.getElementById('main-nav');
+const ticketsLink = nav.querySelector('#nav-tickets a');
+const otherItems = [...nav.querySelectorAll('li')].filter(li => li.id !== 'nav-tickets');
+
+otherItems.forEach(li => {
+  li.addEventListener('mouseenter', () => {
+    ticketsLink.classList.remove('text-white', 'bg-[#ee7202]');
+    ticketsLink.classList.add('text-[#3d6ef9]');
+    console.log("test");
+  });
+});
+
+nav.addEventListener('mouseleave', () => {
+  ticketsLink.classList.remove('text-[#3d6ef9]');
+  ticketsLink.classList.add('text-white', 'bg-[#ee7202]');
+});
